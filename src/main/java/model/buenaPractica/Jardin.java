4ebd4588dc02d10338b6bfcd560ec19528b98e19
jfaundez07 <j.faundez07@ufromail.cl>
2024-08-29 09:09:30 -0400

    Se añaden java conventions en README.md y se reordena la estrcutura de los comentarios en "bunaPractica"

--- src/main/java/model/buenaPractica/Jardin.java
+++ src/main/java/model/buenaPractica/Jardin.java
@@ -1,13 +1,20 @@
 package model.buenaPractica;
 
 public class Jardin {
+    /*
+         parametro planta -> nombre de la planta,
+         parametro requiereAgua -> determina si la planta necesita agua
+    */
+    public void regarPlanta(String planta, Boolean requiereAgua) {
 
-    public void regarPlanta(String planta, Boolean requiereAgua) {  // parametro planta -> nombre de la planta, parametro requiereAgua -> determina si la planta necesita agua
-        if (requiereAgua) {  // Si requiereAgua es verdadero (true) entra en el condicional if
+        if (requiereAgua) {                       /* Si requiereAgua es verdadero (true) entra en el condicional if */
             System.out.println("Regando la planta:  " + planta);
-        } else {  // Si requiereAgua no es verdadero (es decir, es falso), entra en el condicional else
+
+        } else {                                            /*  Si requiereAgua no es verdadero (es decir, es falso), entra en el condicional else */
             System.out.println("La planta " + planta + " no necesita agua en este momento");
         }
+
     }
+
 
 }
